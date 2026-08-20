0397f0b93225f3af994b56244022160a4f802962
Xy123z <puskarbanerjee5@gmail.com>
2026-08-20 14:04:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const WebSocket = require('ws');
 const readline = require('readline');
-
 const wss = new WebSocket.Server({ port: 8080 });
 console.log("Server listening on port 8080...");
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
@@ -11,19 +10,22 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const data = JSON.parse(message);
     if (data.event === "ACK") {
-      console.log("ESP32 Acknowledged! You can now send commands (e.g., 'left', 'right'):");
+      console.log("ESP32 Acknowledged! Tell me howmany steps should I walk?):");
       startCli(ws);
     }
   });
 });
-
+function tokenizer(input){
+const [action="",direction="",angle="0"] = input.split(/\s+\); 
+return {
+  action,
+  direction,
+  angle: Number(angle) || 0
+};                                                              
+}
 function startCli(ws) {
   rl.on('line', (line) => {
     const cmd = line.trim().toLowerCase();
-    if (cmd === 'left' || cmd === 'right') {
-      ws.send(JSON.stringify({ command: cmd }));
-    } else {
-      console.log("Unknown command. Type 'left' or 'right'.");
-    }
+    JSON.stringify(tokenizer(com));
   });
 }
